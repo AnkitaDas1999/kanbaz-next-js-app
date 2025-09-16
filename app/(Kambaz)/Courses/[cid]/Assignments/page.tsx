b1b0955cce10f908ccf1d0753fec8b47fed0cd52
Ankita Das <dasankita1220@gmail.com>
2025-09-16 19:12:28 -0400

    Replaced anchor tag with Link tag

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,22 +11,22 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-          </a>
+          </Link>
           <div>
             Multiple Modules |
             <b> Not available until </b> May 6 at 12:00am |
             <b> Due </b> May 13 at 11:59pm | 100 pts
           </div> </li>
           <li className="wd-assignment-list-item">
-          <a
+          <Link
             href="/Courses/1234/Assignments/124"
             className="wd-assignment-link"
           >
             A2 - CSS + BOOTSTRAP
-          </a>
+          </Link>
           <div>
             Multiple Modules |
             <b> Not available until </b> May 13 at 12:00am |
@@ -32,12 +34,12 @@ export default function Assignments() {
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             href="/Courses/1234/Assignments/125"
             className="wd-assignment-link"
           >
             A3 - JAVASCRIPT + REACT
-          </a>
+          </Link>
           <div>
             Multiple Modules |
             <b> Not available until </b> May 20 at 12:00am |
