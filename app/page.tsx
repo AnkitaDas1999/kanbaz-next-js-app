93ee8d4e26c7ecac37963d979d64ea250e7f5d9e
Ankita Das <dasankita1220@gmail.com>
2025-09-17 18:07:04 -0400

    Added my name in the landing page of the app

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Kambaz</h1>
-      <p>Welcome to Kambaz - Learning Management System</p>
+      <h1>Welcome to Kambaz - Learning Management System</h1>
+      <p>My name is Ankita Das</p>
       <Link href="/Account/Signin">
         Enter Kambaz
       </Link>
