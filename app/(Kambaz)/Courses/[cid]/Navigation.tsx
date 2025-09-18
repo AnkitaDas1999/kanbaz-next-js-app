2151b942da85b286e7a17a5890123dda6118408f
Ankita Das <dasankita1220@gmail.com>
2025-09-17 21:14:47 -0400

    Made quiz and grade links to open in new tab

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -6,12 +6,12 @@ export default function CourseNavigation() {
       <Link href="/Courses/1234/Modules" id="wd-course-modules-link">Modules
         </Link><br/>
       <Link href="https://piazza.com" id="wd-course-piazza-link" target="_blank">Piazza</Link><br/>
-      <Link href="https://www.zoom.com/" id="wd-course-zoom-link">Zoom</Link><br/>
+      <Link href="https://www.zoom.com/" id="wd-course-zoom-link" target="_blank">Zoom</Link><br/>
       <Link href="/Courses/1234/Assignments" id="wd-course-quizzes-link">
           Assignments</Link><br/>
-      <Link href="https://northeastern.instructure.com/courses/225999/quizzes" id="wd-course-assignments-link">Quizzes
+      <Link href="https://northeastern.instructure.com/courses/225999/quizzes" id="wd-course-assignments-link" target="_blank">Quizzes
         </Link><br/>
-      <Link href="https://northeastern.instructure.com/courses/225999/grades" id="wd-course-grades-link">Grades</Link><br/>
+      <Link href="https://northeastern.instructure.com/courses/225999/grades" id="wd-course-grades-link" target="_blank">Grades</Link><br/>
       <Link href="#" id="wd-course-people-link">People</Link><br/>
     </div>
   );}
