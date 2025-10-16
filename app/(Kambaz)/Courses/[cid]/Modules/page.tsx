071483e7c41a72926485e14bda38886686cfbf64
Ankita Das <dasankita1220@gmail.com>
2025-10-15 20:01:00 -0400

    fixed modules error

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useParams } from "next/navigation";
 import * as db from "../../../Database";
@@ -9,13 +9,16 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 
 type Lesson = {
-  id: string;
+  _id: string;
   name: string;
+  description: string;
+  module: string;
 };
 
 type Module = {
-  id: string;
+  _id: string;
   name: string;
+  description: string;
   course: string;
   lessons?: Lesson[];
 };
@@ -26,20 +29,32 @@ export default function Modules() {
 
   return (
     <div>
-      <ModulesControls /><br /><br /><br /><br />
+      <ModulesControls />
+      <br />
+      <br />
+      <br />
+      <br />
       <ListGroup className="rounded-0" id="wd-modules">
         {modules
           .filter((module: Module) => module.course === cid)
           .map((module: Module) => (
-            <ListGroupItem key={module.id} className="wd-module p-0 mb-5 fs-5 border-gray">
+            <ListGroupItem
+              key={module._id}
+              className="wd-module p-0 mb-5 fs-5 border-gray"
+            >
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <ModuleControlButtons />
               </div>
               {module.lessons && (
                 <ListGroup className="wd-lessons rounded-0">
                   {module.lessons.map((lesson: Lesson) => (
-                    <ListGroupItem key={lesson.id} className="wd-lesson p-3 ps-1">
-                      <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                    <ListGroupItem
+                      key={lesson._id}
+                      className="wd-lesson p-3 ps-1"
+                    >
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                      <LessonControlButtons />
                     </ListGroupItem>
                   ))}
                 </ListGroup>
