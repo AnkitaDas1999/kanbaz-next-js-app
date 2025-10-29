254a239b72182bd36e3c42ae0938f38690e72f26
Ankita Das <dasankita1220@gmail.com>
2025-10-29 13:35:58 -0400

    fixing build

--- app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
@@ -2,6 +2,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterReducer";
 
+export const dynamic = "force-dynamic";
+
 export default function CounterRedux() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { count } = useSelector((state: any) => state.counterReducer);
