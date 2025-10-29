b3963505127b596d5081aa3caeb5fa0577819690
Ankita Das <dasankita1220@gmail.com>
2025-10-29 13:21:07 -0400

    added use client for usestate

--- app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import { add } from "./addReducer";
 import { FormControl, Button } from "react-bootstrap";
+
 export default function AddRedux() {
   const [a, setA] = useState(12);
   const [b, setB] = useState(23);
